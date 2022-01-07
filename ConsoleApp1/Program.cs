55c552d4bffc4b094293e73550221d68ee79f5e1
David Carroll <carroll.david.s@gmail.com>
2022-01-07 14:49:42 -0600

    editorconfig to remove spaces (as much as it will let me) - remove braces from foreach statement

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,1 @@
-﻿foreach (var x in Range(1, 100)) { WriteLine($"{(x % 3 == 0 ? "Fizz" : "")}{(x % 5 == 0 ? "Buzz" : "")}{(x % 3 != 0 && x % 5 != 0 ? x : "")}"); }
+﻿foreach(var x in Range(1,100)) WriteLine($"{(x%3==0 ? "Fizz" : "")}{(x%5==0 ? "Buzz" : "")}{(x%3!=0&&x%5!=0 ? x : "")}");
