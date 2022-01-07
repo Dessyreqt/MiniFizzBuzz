be7335a8ec1f6d9abfdc6421601fc66338a12eb6
David Carroll <carroll.david.s@gmail.com>
2022-01-07 14:50:42 -0600

    for instead of foreach

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,1 @@
-﻿foreach(var x in Range(1,100)) WriteLine($"{(x%3==0 ? "Fizz" : "")}{(x%5==0 ? "Buzz" : "")}{(x%3!=0&&x%5!=0 ? x : "")}");
+﻿for(var x = 1;x<101;x++) WriteLine($"{(x%3==0 ? "Fizz" : "")}{(x%5==0 ? "Buzz" : "")}{(x%3!=0&&x%5!=0 ? x : "")}");
